83f03c7dcad983231140bea50d891e1a94829bd7
keel <keel.sike@gmail.com>
2017-08-22 16:01:30 +0800

    update to es6

--- test/test.vlog.js
+++ test/test.vlog.js
@@ -1,20 +1,20 @@
 
 
 'use strict';
-var expect = require('chai').expect;
-var vlog = require('../lib/vlog').instance(__filename);
+const expect = require('chai').expect;
+const vlog = require('../lib/vlog').instance(__filename);
 
 describe('vlog.js', function() {
 
   describe('#ee()', function() {
-    var test = function(callback) {
+    const test = function(callback) {
       callback(vlog.ee(null,'error msg1','testErrA'));
     };
 
     it('should return the error stack', function(done) {
-      var test2 = function(e) {
+      const test2 = function(e) {
         if (e) {
-          var re = vlog.ee(e,'test2 error').stack;
+          const re = vlog.ee(e,'test2 error').stack;
           expect(re.indexOf(']test2 error') > 0).to.be.eql(true);
           expect(re.indexOf(']error msg1') > 0).to.be.eql(true);
         }
